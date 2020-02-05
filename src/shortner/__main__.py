517044b23d2761edbc2d00d6a78c568482033785
Kirill Sumorokov <sumorokov.k@gmail.com>
2020-02-05 15:47:36 +0500

    Rewrite with aiohttp-micro

--- src/shortner/__main__.py
+++ src/shortner/__main__.py
@@ -1,19 +1,10 @@
 import asyncio
 
 import click
-import structlog  # type: ignore
 import uvloop  # type: ignore
+from aiohttp_micro.management.server import server
 
 from shortner.app import AppConfig, init
-from shortner.management.server import server
-
-
-structlog.configure(
-    processors=[
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.JSONRenderer(),
-    ]
-)
 
 
 @click.group()
@@ -23,14 +14,12 @@ def cli(ctx, debug):
     uvloop.install()
     loop = asyncio.get_event_loop()
 
-    logger = structlog.get_logger()
-
     config = AppConfig()
     config.load_from_env()
 
     config.debug = debug
 
-    app = loop.run_until_complete(init("shortner", config, logger))
+    app = loop.run_until_complete(init("shortner", config))
 
     ctx.obj["app"] = app
     ctx.obj["config"] = config
--- src/shortner/app.py
+++ src/shortner/app.py
@@ -1,14 +1,7 @@
-import socket
-
 import config  # type: ignore
-import pkg_resources
-import sentry_sdk
 from aiohttp import web
 from aiohttp_metrics import setup as setup_metrics  # type: ignore
-from sentry_sdk.integrations.aiohttp import AioHttpIntegration
-
-from shortner.handlers import meta
-from shortner.middlewares import catch_exceptions_middleware
+from aiohttp_micro import setup as setup_micro  # type: ignore
 
 
 class AppConfig(config.Config):
@@ -21,32 +14,9 @@ class AppConfig(config.Config):
 async def init(app_name: str, config: AppConfig, logger) -> web.Application:
     app = web.Application()
 
-    app["app_name"] = app_name
-    app["logger"] = logger
-    app["config"] = config
-    app["distribution"] = pkg_resources.get_distribution(app_name)
-    app["hostname"] = socket.gethostname()
-
-    if config.sentry_dsn:
-        sentry_sdk.init(
-            dsn=config.sentry_dsn, integrations=[AioHttpIntegration()]
-        )
-
+    setup_micro(app, app_name, config)
     setup_metrics(app, app_name=app_name)
 
-    app.middlewares.append(catch_exceptions_middleware)  # type: ignore
-
-    app.router.add_routes(
-        [
-            web.get("/-/health", meta.health, name="health"),
-            web.get("/-/meta", meta.index, name="index"),
-        ]
-    )
-
-    app["logger"].info(
-        "Initialize application",
-        app_name=app["app_name"],
-        hostname=app["hostname"],
-    )
+    app["logger"].info("Initialize application")
 
     return app
